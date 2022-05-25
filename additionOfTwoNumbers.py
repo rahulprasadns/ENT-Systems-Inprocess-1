f3526fa9677aa31a7d0c438fbb2819ab414a7b86
Rahul Prasad <nagumantrirahul@gmail.com>
2022-05-25 14:05:49 -0400

    addition of two numbers program

---
+++ additionOfTwoNumbers.py
@@ -0,0 +1,7 @@
+#This is the program that adds two numbers together.
+
+first = int(input("Enter the first number: "))
+second = int(input("Enter the second number: "))
+
+addition = first + second
+print("The addition of two numbers is: ", addition)

e577b408ee65ba88451722d29040db7538162fb8
Rahul Prasad <nagumantrirahul@gmail.com>
2022-05-25 14:18:27 -0400

    python program to subtract two numbers

---
+++ subtractionOfTwoNumbers.py
@@ -0,0 +1,7 @@
+#This is the program that subtracts two numbers together.
+
+first = int(input("Enter the first number: "))
+second = int(input("Enter the second number: "))
+
+subtraction = first - second
+print("The addition of two numbers is: ", subtraction)
